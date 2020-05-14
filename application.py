368517288149ff79c097cc3a76b46c0d1066e37d
Kishor Subedi <kishorsubedi@Kishors-MacBook-Pro.local>
2020-05-14 17:22:58 -0400

    deploying python app from deployment center with pyodbc

--- application.py
+++ application.py
@@ -1,6 +1,30 @@
 from flask import Flask
 app = Flask(__name__)
+import os
+import pyodbc
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello MANN!"
+
+@app.route('/posts', methods=['GET'])
+def home():
+    #getPostsFromDb()
+    #console.log(posts)
+    return "<h1>Queried Azure SQl Datbase in" + os.environ['testPathVariable'] + "kishor /get endpoint</h1><p>FIFI</p>"
+
+
+def getPostsFromDb():
+    server = 'ppacedbserver.database.windows.net'
+    database = 'ppaceDb'
+    username = 'kishbuy238'
+    password = '238aZure'
+    driver= '{ODBC Driver 17 for SQL Server}'
+    cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor()
+    cursor.execute("SELECT * FROM dbo.Customers")
+    row = cursor.fetchone()
+    while row:
+        print (str(row[0]) + " " + str(row[1]))
+        row = cursor.fetchone()
+    
